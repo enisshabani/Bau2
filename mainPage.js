f74b2dc1b90f1b5fd0ab969b188cbc9f86e0faf5
Enis Shabani <81578941+enisshabani@users.noreply.github.com>
2025-08-27 19:18:17 +0200

    Add files via upload

---
+++ mainPage.js
@@ -0,0 +1,57 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    ScrollReveal().reveal('.text', {
+        origin: 'top',
+        distance: '60px',
+        duration: 2500,
+        delay: 100,
+        reset: false
+    });
+
+    ScrollReveal().reveal('.container1', {
+        origin: 'top',
+        distance: '60px',
+        duration: 2500,
+        delay: 100,
+        reset: false
+    });
+
+    ScrollReveal().reveal('.banner2', {
+        origin: 'top',
+        distance: '60px',
+        duration: 2500,
+        delay: 100,
+        reset: false
+    });
+
+    ScrollReveal().reveal('#paragraph', {
+        origin: 'top',
+        distance: '60px',
+        duration: 2500,
+        delay: 100,
+        reset: false
+    });
+
+    ScrollReveal().reveal('h1', {
+        origin: 'top',
+        distance: '60px',
+        duration: 2500,
+        delay: 100,
+        reset: false
+    });
+
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const menuIcon = document.querySelector("#menu-icon");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+
+    if (navLinks.classList.contains("active")) {
+        menuIcon.textContent = "close";
+    } else {
+        menuIcon.textContent = "menu";
+    }
+});
